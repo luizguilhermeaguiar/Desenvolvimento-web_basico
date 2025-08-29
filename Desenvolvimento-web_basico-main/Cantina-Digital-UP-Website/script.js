2e5ac1ed25b6a7607b7cd5741db3e9152b9a65a1
mewveiga <mewveiga.espc@gmail.com>
2025-08-29 19:53:20 -0300

    first update

---
+++ Desenvolvimento-web_basico-main/Cantina-Digital-UP-Website/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  });
